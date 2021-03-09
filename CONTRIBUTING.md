ae8e7781d96806887573ea6d8c4bb9697215b7b5
Elias Nogueira <elias.nogueira@gmail.com>
2021-03-09 21:15:27 +0100

    project structure

---
+++ CONTRIBUTING.md
@@ -0,0 +1,22 @@
+# Contribution guide
+
+## I have an idea, or I want to create an issue
+If you have an idea, suggestion, feature or an issue, please log an [issue](https://github.com/eliasnogueira/selenium-java-lean-test-achitecture/issues).
+
+Select _Bug report_ if tou want log an issue or _Feature request_ if you want to see something new.
+
+Do not forget to add a _label_ on the issue or feature.
+
+## I have enough knowledge in development to collaborate
+Excellent! Thank you to help me out!
+
+You're going to need a few things first:
+* JDK 11+
+
+## Send a pull request
+Please add an explanation about the pull request.
+If there is an issue/feature, please add the link.
+
+Pull requests without explanations will be rejected, so please add:
+* the reason about you're sending the pull request
+* the benefit that your pull request will bring to the application
---
+++ src/test/java/com/eliasnogueira/BaseWeb.java
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.eliasnogueira;
+
+import com.eliasnogueira.driver.config.Configuration;
+import com.eliasnogueira.driver.config.ConfigurationManager;
+import com.eliasnogueira.driver.factory.DriverFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseWeb {
+
+    protected WebDriver driver;
+    protected Configuration configuration;
+
+    @BeforeEach
+    public void preCondition() {
+        configuration = ConfigurationManager.getConfiguration();
+
+        driver = new DriverFactory().createInstance(configuration.browser());
+        driver.get(configuration.url());
+    }
+
+    @AfterEach
+    public void postCondition() {
+        driver.quit();
+    }
+}
