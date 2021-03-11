113c8ed870a447fd3d3418d27f81f962c1893fd3
Elias Nogueira <elias.nogueira@gmail.com>
2021-03-11 08:03:23 +0100

    driver factory to run in a local or remote environment

--- src/main/java/com/eliasnogueira/driver/DriverFactory.java
+++ src/main/java/com/eliasnogueira/driver/DriverFactory.java
@@ -24,14 +24,20 @@
 
 package com.eliasnogueira.driver;
 
-import com.eliasnogueira.driver.factory.LocalDriverFactory;
-import com.eliasnogueira.driver.factory.RemoteDriverFactory;
 import com.eliasnogueira.exceptions.TargetNotValidException;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.eliasnogueira.config.ConfigurationManager.configuration;
 
 public class DriverFactory {
+
+    private static final Logger logger = Logger.getLogger("com.eliasnogueira");
 
     public WebDriver createInstance(String browser) {
         Target target = Target.valueOf(configuration().target().toUpperCase());
@@ -39,15 +45,31 @@ public class DriverFactory {
 
         switch (target) {
             case LOCAL:
-                webdriver = new LocalDriverFactory().createInstance(browser);
+                webdriver = BrowserFactory.valueOf(browser.toUpperCase()).createDriver();
                 break;
             case REMOTE:
-                webdriver = new RemoteDriverFactory().createInstance(browser);
+                webdriver = createRemoteInstance(BrowserFactory.valueOf(browser.toUpperCase()).getOptions());
                 break;
             default:
                 throw new TargetNotValidException(target.toString());
         }
         return webdriver;
+    }
+
+    private RemoteWebDriver createRemoteInstance(MutableCapabilities capability) {
+        RemoteWebDriver remoteWebDriver = null;
+        try {
+            String gridURL = String.format("http://%s:%s", configuration().gridUrl(), configuration().gridPort());
+
+            remoteWebDriver = new RemoteWebDriver(new URL(gridURL), capability);
+        } catch (java.net.MalformedURLException e) {
+            logger.log(Level.SEVERE, "Grid URL is invalid or Grid is not available");
+            logger.log(Level.SEVERE, String.format("Browser: %s", capability.getBrowserName()), e);
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, String.format("Browser %s is not valid or recognized", capability.getBrowserName()), e);
+        }
+
+        return remoteWebDriver;
     }
 
     enum Target {
